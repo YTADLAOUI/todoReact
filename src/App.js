69931593b77abd1d536fae8fea53c69f2ecdb5a8
youssef tadlaoui <usftadlaoui@gmail.com>
2023-08-21 18:28:03 +0100

    font&m&p&box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoWarapper } from './components/TodoWrapper';
 
 function App() {
   return (
     <div className="App">
-     
+     <TodoWarapper/>
     </div>
   );
 }
